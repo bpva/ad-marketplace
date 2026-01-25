47c85c62cc836f3d4f512dd5e2c2bd6cf5444979
bpva <bpva@protonmail.com>
2026-01-25 23:45:20 +0200

    add responder

---
+++ internal/http/respond/respond.go
@@ -0,0 +1,48 @@
+package respond
+
+import (
+	"encoding/json"
+	"errors"
+	"log/slog"
+	"net/http"
+
+	"github.com/bpva/ad-marketplace/internal/dto"
+)
+
+func OK(w http.ResponseWriter, data any) {
+	writeJSON(w, http.StatusOK, data)
+}
+
+func Created(w http.ResponseWriter, data any) {
+	writeJSON(w, http.StatusCreated, data)
+}
+
+func NoContent(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func Err(w http.ResponseWriter, log *slog.Logger, err error) {
+	var apiErr *dto.APIError
+	if !errors.As(err, &apiErr) {
+		log.Error("internal error", "error", err)
+		apiErr = dto.ErrInternalError
+	} else if apiErr.Status() >= 500 {
+		log.Error("server error", "error", err)
+	} else {
+		log.Warn("client error", "error", err)
+	}
+
+	resp := dto.ErrorResponse{
+		ErrorCode: apiErr.Code(),
+		Details:   apiErr.Details(),
+	}
+	writeJSON(w, apiErr.Status(), resp)
+}
+
+func writeJSON(w http.ResponseWriter, status int, data any) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		slog.Error("failed to encode response", "error", err)
+	}
+}
