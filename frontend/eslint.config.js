691619763380a2739155cb815fc9ea2c57cf2947
bpva <bpva@protonmail.com>
2026-01-30 02:57:44 +0200

    add prettier

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,20 +1,25 @@
-import js from '@eslint/js'
-import tseslint from 'typescript-eslint'
-import reactHooks from 'eslint-plugin-react-hooks'
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import reactHooks from "eslint-plugin-react-hooks";
+import prettier from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
 
 export default tseslint.config(
   js.configs.recommended,
   ...tseslint.configs.recommended,
+  prettierConfig,
   {
     plugins: {
-      'react-hooks': reactHooks,
+      "react-hooks": reactHooks,
+      prettier: prettier,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      '@typescript-eslint/no-unused-vars': 'error',
+      "@typescript-eslint/no-unused-vars": "error",
+      "prettier/prettier": "error",
     },
   },
   {
-    ignores: ['dist/**'],
-  }
-)
+    ignores: ["dist/**"],
+  },
+);
