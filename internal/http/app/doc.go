60ca00165fb9f78c87e4ae03544ad2a8a6d806a0
bpva <bpva@protonmail.com>
2026-01-31 14:37:27 +0200

    add openapi root

---
+++ internal/http/app/doc.go
@@ -0,0 +1,10 @@
+// Package app API handlers
+//
+//	@title			Ad Marketplace API
+//	@version		1.0
+//	@BasePath		/api/v1
+//
+//	@securityDefinitions.apikey	BearerAuth
+//	@in							header
+//	@name						Authorization
+package app
